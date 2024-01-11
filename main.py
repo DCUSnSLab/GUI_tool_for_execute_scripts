3d3429747497f3551b7fe5a95616a9cddbbb76db
choeyujeong911 <yujeongchoe@gmail.com>
2024-01-11 16:24:28 +0900

    실행 테스트 파일 추가

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -7,7 +8,11 @@ class GUITool(QWidget):
         super().__init__()
         self.btn_list = [QPushButton('START'), QPushButton('STOP')]
         self.btn_list[0].setMaximumHeight(500)
+        self.btn_list[0].clicked.connect(self.click_startBtn)
         self.btn_list[1].setMaximumHeight(500)
+        self.btn_list[1].clicked.connect(self.click_stopBtn)
+        self.status_label = QLabel('')
+
         # 버튼을 비활성화하는 메소드를 사용합니다.
         self.btn_list[1].setEnabled(False)
         self.init_UI()
@@ -18,7 +23,7 @@ class GUITool(QWidget):
         self.main_layout.setRowStretch(1, 1)
         self.main_layout.addWidget(self.btn_list[0], 0, 0)
         self.main_layout.addWidget(self.btn_list[1], 0, 1)
-        self.main_layout.addWidget(QLabel('TEST'), 1, 0)
+        self.main_layout.addWidget(self.status_label, 1, 0)
 
         self.setLayout(self.main_layout)
         self.setWindowTitle('GUI Tool')
@@ -32,6 +37,16 @@ class GUITool(QWidget):
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+    def click_startBtn(self):
+        self.btn_list[0].setEnabled(False)
+        self.btn_list[1].setEnabled(True)
+        self.status_label.setText('RUN > > ')
+
+    def click_stopBtn(self):
+        self.btn_list[0].setEnabled(True)
+        self.btn_list[1].setEnabled(False)
+        self.status_label.setText('KILL and SAVE !')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
---
+++ test2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//#include <unistd.h>
+#include <windows.h>
+
+int main() {
+    printf("TEST2 FILE is RUNNING ! **\n");
+    printf("Bye World !\n");
+    Sleep(7);
+    // sleep(7);
+    return 0;
+}
