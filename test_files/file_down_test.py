44cb4b55f1372eb2886403b0373b745f91cb8c19
choeyujeong911 <yujeongchoe20@gmail.com>
2024-01-29 20:54:16 +0900

    예시 실행파일 작성 완료

--- test_files/file_down_test.py
+++ test_files/file_down_test.py
@@ -1,6 +1,24 @@
 import os
 import sys
-import subprocess
+import time
 import signal
 
 # len(sys.argv) : 파라미터 개수(파라미터없이 실행만 시킬 경우, 개수는 1.)
+def exit_handler(signum, frame):
+    # print("Handler is exiting due to signal:", signum)
+    print("Finished !")
+    filepath = os.getcwd()
+    print(filepath)
+    filename = os.path.join("test_files", "result_files", str(time.strftime("%Y-%m-%d__%A__%H:%M:%S")) + ".txt")
+    print(filename)
+    file = open(filename, "w")
+    file.write("X_coord = " + str(sys.argv[1]) + "\n")
+    file.write("Y_coord = " + str(sys.argv[2]) + "\n")
+    file.close()
+    sys.exit()
+
+signal.signal(signal.SIGTERM, exit_handler)
+
+while True:
+    print("Hello World")
+    time.sleep(1)
