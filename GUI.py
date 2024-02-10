1ed429286067cc98a7b0a4908811e116e26e93bd
choeyujeong911 <yujeongchoe20@gmail.com>
2024-02-10 22:20:58 +0900

    UI 디자인 완료

--- GUI.py
+++ GUI.py
@@ -20,6 +20,9 @@ PREVIEW_SCALE = float(2 / 3)
 ROI_WIDTH = int(1024 * PREVIEW_SCALE)
 ROI_HEIGHT = int(512 * PREVIEW_SCALE)
 
+WIDGET_HEIGHT = int(60)
+WIDGET_MARGIN = int(10)
+
 FROM_PATH = "./Result"
 
 class MyMainWindow(QWidget):
@@ -28,43 +31,25 @@ class MyMainWindow(QWidget):
         self.child_process = None
         self.to_path = ".."
         self.data_list = []
-        self.clk_x = int(30 + float(PREVIEW_WIDTH / 2) - float(ROI_WIDTH / 2))
-        self.clk_y = int(30 + float(PREVIEW_HEIGHT / 2) - float(ROI_HEIGHT / 2))
 
-        self.top_list = [QPushButton("UP-DOWN REVERSE"), QPushButton("SHUTDOWN COMPUTER")]
-        self.top_list[0].clicked.connect(self.click_reverse)
-        self.coord_label = QLabel("p1(x, y) : (" + str(self.clk_x) + ", " + str(self.clk_y) + ")", self)
-        self.top_list[1].clicked.connect(self.click_shutdown)
+        self.btn_list = [QPushButton("SHUTDOWN", self), QPushButton("UP-DOWN REVERSE", self),
+                         QPushButton("SELECT PATH", self), QPushButton("DOWNLOAD", self),
+                         QPushButton("STOP", self), QPushButton("START", self)]
+        self.label_list = [QLabel("> ROI Box Coordinates: (   \t\t, \t\t   )", self),
+                           QLabel("> Path to Download Data Files to", self)]
+        self.x_info = QLineEdit(" -", self)
+        self.y_info = QLineEdit(" -", self)
+        self.path_info = QLineEdit(" -", self)
 
-        self.btn_list = [QPushButton("START"), QPushButton("STOP"), QPushButton("DOWNLOAD")]
-        self.btn_list[0].clicked.connect(self.click_start)
-        self.btn_list[1].clicked.connect(self.click_stop)
-        self.btn_list[2].clicked.connect(self.click_download)
-
-        self.select_btn = QPushButton("SELECT")
-        self.select_btn.clicked.connect(self.click_select)
-        self.info_layout = QHBoxLayout()
-        self.info_layout.addWidget(QLabel("Path to Download Data Files to"))
-        self.info_layout.addWidget(self.select_btn)
-        self.path_label = QLabel()
-        self.path_label.setWindowFilePath(self.to_path)
-        self.download_layout = QVBoxLayout()
-        self.download_layout.addLayout(self.info_layout, 1)
-        self.download_layout.addWidget(self.path_label, 1)
-
-        # 크기 정책 설정
-        for btn in self.top_list + self.btn_list + [self.select_btn]:
-            btn.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.init_UI()
-
     def init_UI(self):
-        self.top_layout = QHBoxLayout()
-        self.top_layout.addWidget(self.top_list[0], 1)
-        self.top_layout.addWidget(self.coord_label, 2)
-        self.top_layout.addWidget(self.top_list[1], 1)
 
-        self.cam_preview = QLabel()
-        self.cam_preview.setGeometry(320, 200, PREVIEW_WIDTH, PREVIEW_HEIGHT)
+        self.logo = QLabel(self)
+        self.logo.setGeometry(0, 0, 1920, 270)
+        self.logo.setPixmap(QPixmap.fromImage(QImage("logo.png").scaled(self.logo.size())))
+
+        self.img_label = QLabel(self)
+        self.img_label.setGeometry(20, 270, 1280, 720)
         self.installEventFilter(self)
         timer = QTimer(self)
         timer.timeout.connect(self.update_frame)
@@ -72,32 +57,29 @@ class MyMainWindow(QWidget):
         self.capture = cv2.VideoCapture(0)
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_WIDTH)
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
-        self.scene = QGraphicsScene()
-        self.scene.setBackgroundBrush(Qt.transparent)
-        self.view = QGraphicsView(self.scene)
-        self.view.setStyleSheet("background: transparent;")
-        self.view.setSceneRect(0, 0, PREVIEW_WIDTH, PREVIEW_HEIGHT)
-        self.view.setGeometry(320, 200, PREVIEW_WIDTH, PREVIEW_HEIGHT)
+        self.scene = QGraphicsScene(self)
+        self.scene.setBackgroundBrush(Qt.transparent)  # 배경을 투명하게 설정
+        self.view = QGraphicsView(self.scene, self)
+        self.view.setStyleSheet("background: transparent;")  # 배경을 투명하게 설정
+        self.view.setSceneRect(0, 0, CAM_WIDTH + 60, CAM_HEIGHT + 120)
+        self.view.setGeometry(0, 0, CAM_WIDTH + 60, CAM_HEIGHT + 120)
+        self.view.setFixedSize(CAM_WIDTH + 60, CAM_HEIGHT + 120)
         self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.roi_box = QGraphicsRectItem(QRectF(int(320 + float(PREVIEW_WIDTH / 2) - float(ROI_WIDTH / 2)), int(30 + float(PREVIEW_HEIGHT / 2) - float(ROI_HEIGHT / 2)), ROI_WIDTH, ROI_HEIGHT))
-        self.roi_box.setBrush(QBrush(Qt.transparent))
-        self.roi_box.setPen(QPen(Qt.green, 10, Qt.SolidLine))
-        self.scene.addItem(self.roi_box)
-        self.is_it_reversed = int(0)
+        self.is_it_reversed = 0
 
-        self.btn_layout = QHBoxLayout()
-        self.btn_layout.addWidget(self.btn_list[0], 1)
-        self.btn_layout.addWidget(self.btn_list[1], 1)
-        self.btn_layout.addWidget(self.btn_list[2], 1)
-        self.btn_layout.addLayout(self.download_layout, 4)
+        self.btn_list[0].setGeometry(1320, 270, 480, WIDGET_HEIGHT)
+        self.btn_list[1].setGeometry(1320, 270 + (WIDGET_HEIGHT + WIDGET_MARGIN) * 1, 480, WIDGET_HEIGHT)
+        self.label_list[0].setGeometry(1320, 270 + (WIDGET_HEIGHT + WIDGET_MARGIN) * 2, 480, WIDGET_HEIGHT)
+        self.x_info.setGeometry(1320 + 180, 270 + (WIDGET_HEIGHT + WIDGET_MARGIN) * 2 + 10, 100, WIDGET_HEIGHT - 20)
+        self.y_info.setGeometry(1320 + 320, 270 + (WIDGET_HEIGHT + WIDGET_MARGIN) * 2 + 10, 100, WIDGET_HEIGHT - 20)
+        self.btn_list[2].setGeometry(1320, 270 + (WIDGET_HEIGHT + WIDGET_MARGIN) * 3, 480, WIDGET_HEIGHT)
+        self.label_list[1].setGeometry(1320, 270 + (WIDGET_HEIGHT + WIDGET_MARGIN) * 4, 480, WIDGET_HEIGHT)
+        self.path_info.setGeometry(1320, 270 + (WIDGET_HEIGHT + WIDGET_MARGIN) * 5, 480, WIDGET_HEIGHT - 20)
+        self.btn_list[3].setGeometry(1320, 270 + (WIDGET_HEIGHT + WIDGET_MARGIN) * 6, 480, WIDGET_HEIGHT * 2)
+        self.btn_list[4].setGeometry(1320, 270 + (WIDGET_HEIGHT + WIDGET_MARGIN) * 7 + WIDGET_HEIGHT, 235, WIDGET_HEIGHT * 2 + 50)
+        self.btn_list[5].setGeometry(1320 + 245, 270 + (WIDGET_HEIGHT + WIDGET_MARGIN) * 7 + WIDGET_HEIGHT, 235, WIDGET_HEIGHT * 2 + 50)
 
-        self.main_layout = QVBoxLayout()
-        self.main_layout.addLayout(self.top_layout, 1)
-        self.main_layout.addWidget(self.cam_preview, 10)
-        self.main_layout.addLayout(self.btn_layout, 1)
-
-        self.setLayout(self.main_layout)
         self.setWindowTitle("POTHOLE DETECTION")
         self.setFont(QFont("Arial", 11))
         self.showMaximized()
@@ -127,9 +109,10 @@ class MyMainWindow(QWidget):
             # q_img.scaledToWidth(PREVIEW_WIDTH)
             # q_img.scaledToHeight(PREVIEW_HEIGHT)
             # QLabel에 이미지를 표시합니다.
-            pixmap = QPixmap.fromImage(q_img)
-            pixmap = pixmap.scaled(self.cam_preview.size())
-            self.cam_preview.setPixmap(pixmap)
+            # pixmap = QPixmap.fromImage(q_img)
+            # pixmap = pixmap.scaled(self.cam_preview.size())
+            # self.cam_preview.setPixmap(pixmap)
+            self.img_label.setPixmap(QPixmap.fromImage(q_img))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
